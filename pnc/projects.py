d45eda882d2b2a579cc49e59536d66e3b089da8b
Tom Hauser <thauser@redhat.com>
2015-07-17 13:35:14 -0400

    use clearer parameter names when possible. correct docstrings

--- pnc/projects.py
+++ pnc/projects.py
@@ -43,7 +43,7 @@ def _project_exists(search_id):
     :return: True if a project with search_id exists
     """
     response = ProjectsApi(utils.get_api_client()).getSpecific(id=search_id)
-    if (response.ok):
+    if response.ok:
         return True
     return False
 
@@ -54,28 +54,28 @@ def _project_exists(search_id):
 @arg("-i","--issue_url", help="Issue tracker URL for the new project")
 @arg("-l","--license_id", help="License ID for the new project")
 def create_project(name, configuration_ids=None, description=None, issue_url=None, project_url=None, license_id=None):
-    "Create a new project"
+    """Create a new project"""
     project = _create_project_object(name, description, issue_url, project_url, configuration_ids,license_id)
     response = utils.pretty_format_response(ProjectsApi(utils.get_api_client()).createNew(body=project).json())
     print(response)
 
-@arg("id", help="ID for the project that will be updated")
+@arg("project-id", help="ID for the project that will be updated")
 @arg("-n","--name", help="Name for the project")
 @arg("-cids","--configuration-ids", help="List of configuration IDs this project should be associated with")
 @arg("-desc","--description", help="Detailed description of the new project")
 @arg("-purl","--project_url", help="SCM Url for the project")
 @arg("-iurl","--issue_url", help="Issue tracker URL for the new project")
 @arg("-l","--license_id", help="License ID for the new project")
-def update_project(id, name=None, description=None, issue_url=None, project_url=None, configuration_ids=None,license_id=None):
+def update_project(project_id, name=None, description=None, issue_url=None, project_url=None, configuration_ids=None,license_id=None):
     project = _create_project_object(name, description, issue_url, project_url, configuration_ids, license_id)
-    if _project_exists(id):
-        response = ProjectsApi(utils.get_api_client()).update(id=id,body=project)
+    if _project_exists(project_id):
+        response = ProjectsApi(utils.get_api_client()).update(id=project_id,body=project)
         if response.ok:
-            print("Project {0} successfully updated.").format(id)
+            print("Project {0} successfully updated.").format(project_id)
         else:
-            print("Updating project with id {0} failed").format(id)
+            print("Updating project with id {0} failed").format(project_id)
     else:
-        print("No project with id {0} exists.").format(id)
+        print("No project with id {0} exists.").format(project_id)
 
 @arg("-id","--id",help="ID of the project to retrieve")
 @arg("-n","--name", help="Name of the project to retrieve")
@@ -102,23 +102,23 @@ def delete_project(id=None, name=None):
         if not _project_exists(id):
             print("No project with id {0} exists.").format(id)
             return
-        project_id = id
+        id = id
     elif name:
-        project_id = _get_project_id_by_name(name)
-        if not project_id:
+        id = _get_project_id_by_name(name)
+        if not id:
             print("There is no project with name {0}.").format(name)
             return
     else:
         print("Either a project name or id is required.")
         return
 
-    response = ProjectsApi(utils.get_api_client()).deleteSpecific(id=project_id)
-    if (response.ok):
-        print("Project {0} successfully deleted.").format(project_id)
+    response = ProjectsApi(utils.get_api_client()).deleteSpecific(id=id)
+    if response.ok:
+        print("Project {0} successfully deleted.").format(id)
     else:
-        print("Failed to delete Project {0}").format(project_id)
+        print("Failed to delete Project {0}").format(id)
 
 def list_projects():
-    "Get a JSON object containing existing projects"
+    """Get a JSON object containing existing projects"""
     response = ProjectsApi(utils.get_api_client()).getAll()
     print(utils.pretty_format_response(response.json()))
