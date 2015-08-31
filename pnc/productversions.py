2ec13fd11a32552cc45fc49a0f064b83215a1f12
Tom Hauser <thauser@redhat.com>
2015-08-31 12:53:56 -0400

    update productversions with better function / REST api call separation

--- pnc/productversions.py
+++ pnc/productversions.py
@@ -5,7 +5,9 @@ import utils
 
 __author__ = 'thauser'
 
-def create_product_version_object(version, product_id, current_milestone_id, product_milestones, build_config_sets, product_releases):
+
+def create_product_version_object(version, product_id=None, current_milestone_id=None, product_milestones=None,
+                                  build_config_sets=None, product_releases=None):
     """
     Create an instance of the ProductVersion object
     :param version:
@@ -25,14 +27,17 @@ def create_product_version_object(version, product_id, current_milestone_id, pro
     if product_releases: created_version.productReleases = product_releases
     return created_version
 
-def _version_exists(version_id):
-    return ProductversionsApi(utils.get_api_client()).getSpecific(id=version_id).ok
 
-@arg("-a","--attributes", help="Comma separated list of attributes to print for each product-version")
+def _version_exists(version_id):
+    return get_specific(version_id).ok
+
+
+@arg("-a", "--attributes", help="Comma separated list of attributes to print for each product-version")
 def list_product_versions(attributes=None):
-    product_versions = ProductversionsApi(utils.get_api_client()).getAll().json()
+    product_versions = get_all().json()
     if attributes is not None:
-        utils.print_matching_attribute(product_versions, attributes, client.models.ProductVersion.ProductVersion().attributeMap)
+        utils.print_matching_attribute(product_versions, attributes,
+                                       client.models.ProductVersion.ProductVersion().attributeMap)
     else:
         utils.print_by_key(product_versions)
 
@@ -43,17 +48,15 @@ def list_product_versions(attributes=None):
 @arg("-pm", "--product-milestones", help="List of milestone IDs to associate with the new version")
 @arg("-bc", "--build-configuration-sets", help="List of build configuration set IDs to associate with the new version")
 @arg("-pr", "--product-releases", help="List of release IDs to associate with this version")
-
-def create_product_version(product_id, version, current_milestone=None, product_milestones=None, build_configuration_sets=None, product_releases=None):
-    version = create_product_version_object(version, product_id, current_milestone, product_milestones, build_configuration_sets, product_releases)
-    response = ProductversionsApi(utils.get_api_client()).createNewProductVersion(body=version)
+def create_product_version(product_id, version, current_milestone=None, product_milestones=None,
+                           build_configuration_sets=None, product_releases=None):
+    version = create_product_version_object(version, product_id, current_milestone, product_milestones,
+                                            build_configuration_sets, product_releases)
+    response = create(version)
     if not response.ok:
-        utils.print_error(__name__,response)
+        utils.print_error(__name__, response)
         return
-
-    new_version = response.json()
-    utils.print_by_key(new_version)
-    return new_version
+    utils.print_by_key(response.json())
 
 # TODO: allow resolution / search by version
 @arg("id", help="ID of the product version to retrieve")
@@ -64,16 +67,18 @@ def get_product_version(id):
     :return:
     """
     if id:
-        response = ProductversionsApi(utils.get_api_client()).getSpecific(id=id)
-        if response.ok:
-            print(utils.pretty_format_response(response.json()))
-        else:
-            print("A product version with id {0} doesn't exist.".format(id))
+        response = get_specific(id)
+        if not response.ok:
+            utils.print_error(__name__,response)
+            return
+        print(utils.print_by_key(response.json()))
     else:
         print("A product version id is required")
 
+
 # TODO: how should constraints be defined? Can a new productId be specified?
-def update_product_version(id, version=None, current_product_milestone=None, product_milestones=None, build_config_sets=None, product_releases=None):
+def update_product_version(id, version=None, current_product_milestone=None, product_milestones=None,
+                           build_config_sets=None, product_releases=None):
     """
     Replace the product version with id with a new product version with provided values
     :param id: id of ProductVersion to replace
@@ -87,12 +92,35 @@ def update_product_version(id, version=None, current_product_milestone=None, pro
     """
     if not _version_exists(id):
         print("A product version with id {0} doesn't exist.".format(id))
-    else:
-        version = create_product_version_object(version, current_product_milestone, product_milestones, build_config_sets, product_releases)
-        response = ProductversionsApi(utils.get_api_client()).update(id=id, body=version)
-        if response.ok:
-            print("Update of version with id {0} successful.".format(id))
-            print(response)
-        else:
-            utils.print_error(__name__,response)
-            print(response)
+        return
+
+    updated_version = create_product_version_object(version, current_product_milestone, product_milestones,
+                                            build_config_sets, product_releases)
+    response = update(id, updated_version)
+    if not response.ok:
+        utils.print_error(__name__, response)
+        return
+
+    print("Update of version with id {0} successful.".format(id))
+
+
+
+def get_all():
+    return ProductversionsApi(utils.get_api_client()).getAll()
+
+
+def get_specific(version_id):
+    return ProductversionsApi(utils.get_api_client()).getSpecific(id=version_id)
+
+
+def create(version):
+    return ProductversionsApi(utils.get_api_client()).createNewProductVersion(body=version)
+
+
+def update(version_id, version):
+    return ProductversionsApi(utils.get_api_client()).update(id=version_id, body=version)
+
+#not implemented in rest
+def delete():
+    pass
+
