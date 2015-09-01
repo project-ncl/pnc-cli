3b40e22bafeff638666f97932f2687b921f25884
Tom Hauser <thauser@redhat.com>
2015-09-01 12:05:34 -0400

    refactor products module

--- pnc/products.py
+++ pnc/products.py
@@ -24,7 +24,20 @@ def _create_product_object(name, description=None, abbreviation=None, product_co
     if system_code: created_product.pgmSystemName = system_code
     return created_product
 
-
+def get_product_id(id,name):
+    if id:
+        prod_id = id
+        if not product_exists(prod_id):
+            print("No product with id {0} exists.".format(prod_id))
+            return
+    elif name:
+        prod_id = get_product_id_by_name(name)
+        if not prod_id:
+            print("No product with name {0} exists.".format(name))
+            return
+    else:
+        print("Either a product name or ID is required.")
+    return prod_id
 
 def get_product_id_by_name(search_name):
     """
@@ -57,10 +70,7 @@ def create_product(name, description=None, abbreviation=None, product_code=None,
     """Define a new product"""
     product = _create_product_object(name, description, abbreviation, product_code, system_code)
     response = create(product)
-    if not response.ok:
-        utils.print_error(sys._getframe().f_code.co_name,response)
-        return
-    utils.print_by_key(response.json())
+    utils.print_json_result(sys._getframe().f_code.co_name,response)
 
 @arg("product-id", help="ID of the product to update")
 @arg("-n","--name", help="New name for the product")
@@ -71,7 +81,7 @@ def create_product(name, description=None, abbreviation=None, product_code=None,
 def update_product(product_id, name=None, description=None, abbreviation=None, product_code=None, system_code=None):
     """Update a product with the given id. Only provide values to update."""
     product = _create_product_object(name, description, abbreviation, product_code, system_code)
-    if _product_exists(product_id):
+    if product_exists(product_id):
         response = update(product_id, product)
         if not response.ok:
             utils.print_error(sys._getframe().f_code.co_name,response)
@@ -81,66 +91,37 @@ def update_product(product_id, name=None, description=None, abbreviation=None, p
     else:
         print("There is no product with id {0}.").format(product_id)
 
-@arg("-n","--name", help="Name of the product to retrieve")
 @arg("-i","--id", help="ID of the product to retrieve")
-def get_product(name=None, id=None):
+@arg("-n","--name", help="Name of the product to retrieve")
+def get_product(id=None, name=None):
     """List information on a specific product."""
-    if id:
-        prod_id = id
-        if not _product_exists(prod_id):
-            print("No product with id {0} exists.".format(id))
-            return
-    elif name:
-        prod_id = _get_product_id_by_name(name)
-        if not prod_id:
-            print("No product with name {0} exists.".format(name))
-            return
-    else:
-        print("Either a product name or ID is required.")
+    prod_id = get_product_id(id,name)
+    if not prod_id:
+        return
+    response = get_specific(prod_id)
+    utils.print_json_result(sys._getframe().f_code.co_name,
+                            response)
 
-    product = get_specific(prod_id).json()
-    utils.print_by_key(product)
-
-@arg("-n","--name", help="Name of the product to retrieve versions from")
 @arg("-i","--id", help="ID of the product to retrieve versions from")
+@arg("-n","--name", help="Name of the product to retrieve versions from")
 @arg("-a","--attributes", help="Comma separated list of attributes to print for each product version")
-def list_versions_for_product(name=None, id=None, attributes=None):
-    valid_attributes = client.models.ProductVersion.ProductVersion().attributeMap
-    if id:
-        response = get_product_versions(id)
-        if response.ok:
-            product_versions = response.json()
-            if attributes is not None:
-                utils.print_matching_attribute(product_versions, attributes, valid_attributes)
-            else:
-                utils.print_by_key(product_versions)
-        else:
-            print("No product with id {0} exists.".format(id))
-    elif name:
-        found_id = _get_product_id_by_name(name)
-        response = get_product_versions(found_id)
-        if found_id:
-            product_versions = response.json()
-            if attributes is not None:
-                utils.print_matching_attribute(product_versions, attributes, valid_attributes)
-            else:
-                utils.print_by_key(product_versions)
-        else:
-            print("No product with name {0} exists.".format(name))
-    else:
-        print("Either a product name or ID is required.")
+def list_versions_for_product(id=None, name=None, attributes=None):
+    prod_id = get_product_id(id,name)
+    if not prod_id:
+        return
+    response = get_product_versions(prod_id)
+    utils.print_json_result(sys._getframe().f_code.co_name,
+                            response,
+                            attributes,
+                            client.models.ProductVersion.ProductVersion().attributeMap)
 
 @arg("-a","--attributes", help="Comma separated list of attributes to print for each product")
 def list_products(attributes=None):
     response = get_all()
-    if not response.ok:
-        utils.print_error(sys._getframe().f_code.co_name,response)
-        return
-    products = response.json()
-    if attributes is not None:
-        utils.print_matching_attribute(products, attributes, client.models.Product.Product().attributeMap)
-    else:
-        utils.print_by_key(products)
+    utils.print_json_result(sys._getframe().f_code.co_name,
+                            response,
+                            attributes,
+                            client.models.Product.Product().attributeMap)
 
 def get_all():
     return ProductsApi(utils.get_api_client()).getAll()
