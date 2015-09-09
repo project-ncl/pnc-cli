3d6c5ca1c66a552ab942b6d9c56820ae312fade6
Tom Hauser <thauser@redhat.com>
2015-09-09 14:40:31 -0400

    update utils, culling dumb / bad functions, updating with useful

--- pnc/utils.py
+++ pnc/utils.py
@@ -1,4 +1,6 @@
 import ConfigParser
+import random
+import string
 import swagger_client
 import os
 
@@ -16,48 +18,8 @@ if not found:
 pnc_rest_url = config.get('PNC', 'restEndpoint')
 apiclient = swagger_client.api_client.ApiClient(pnc_rest_url)
 
-def print_json_result(caller, response, attributes=None, valid_attributes=None):
-    if not response.ok:
-        print_error(caller, response)
-        return
-    if attributes:
-        print_matching_attribute(response.json(), attributes, valid_attributes)
-    else:
-        print_by_key(response.json())
-
-def print_matching_attribute(json, attributes, valid_attributes):
-    attr_list = attributes.split(",")
-    for a in attr_list:
-        if a not in valid_attributes:
-            print("Invalid attribute. Choose from the following list:")
-            print('\n'.join(key for key in valid_attributes))
-            return
-    result = retrieve_keys(json, attr_list)
-    print('\n'.join(str(r[attr]) for r in result for attr in attr_list))
-
-
-def print_by_key(json):
-    """
-    print json objects in the form "key: dict[key]" on each line for each key
-    :param json:
-    :return:
-    """
-    if type(json) is list:
-        for item in json:
-            print('\n'.join(key + ": " + str(item[key]) for key in item.keys()))+"\n"
-    else:
-        print('\n'.join(key + ": " + str(json[key]) for key in json.keys()))+'\n'
-
-def retrieve_keys(input_json, keys):
-    if type(input_json) is list:
-        final_dict = [{key : r[key] for key in keys} for r in input_json]
-    else:
-        final_dict = {key : input_json[key] for key in keys}
-    return final_dict
-
-def print_error(func_name, reason):
-    print(func_name + " failed: " + str(reason))
-
 def get_api_client():
     return apiclient
 
+def gen_random_name():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
