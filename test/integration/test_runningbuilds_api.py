4078f6fb11ddf6455a9845875b823d69c4f3fc14
Tom Hauser <thauser@redhat.com>
2015-09-10 16:37:03 -0400

    make sure we don't encounter the 409 conflict status for same BC running

--- test/integration/test_runningbuilds_api.py
+++ test/integration/test_runningbuilds_api.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from pnc import utils
 from pnc.swagger_client.apis.buildconfigurations_api import BuildconfigurationsApi
 from pnc.swagger_client.apis.runningbuildrecords_api import RunningbuildrecordsApi
@@ -8,12 +9,16 @@ running_api = RunningbuildrecordsApi(utils.get_api_client())
 
 def test_get_all():
     # start a build so that a build is running
-    configs_api.trigger(id=1)
+    configs_api.trigger(id=1).content
     running_builds = running_api.get_all().content
+    time.sleep(5)
     assert running_builds is not None
 
 def test_get_specific():
     build = configs_api.trigger(id=1).content
     running_build = running_api.get_specific(id=build.id)
+    time.sleep(5)
     assert running_build is not None
+
+
 
