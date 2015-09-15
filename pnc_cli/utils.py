a530047a76c5083caf61d9dec2e41572d69aecdf
Tom Hauser <thauser@redhat.com>
2015-09-15 15:05:15 -0400

    Revert "refactor some methods in utils"
    This reverts commit 77d140879c78bd66f8f3a1ba6ed017f789509974.

--- pnc_cli/utils.py
+++ pnc_cli/utils.py
@@ -2,31 +2,27 @@ import ConfigParser
 import random
 import string
 import re
-import errno
 import swagger_client
 import os
 
 __author__ = 'thauser'
 
 config = ConfigParser.ConfigParser()
-configfile = os.path.expanduser("~")+"/.config/pnc-cli/pnc-cli.conf"
-configdir = os.path.dirname(configfile)
-try:
-    os.makedirs(configdir)
-except OSError, e:
-    if e.errno != errno.EEXIST:
-        raise
-found = config.read(os.path.join(configfile))
+configfilename = os.path.expanduser("~")+ "/.config/pnc-cli/pnc-cli.conf"
+found = config.read(os.path.join(configfilename))
 if not found:
     config.add_section('PNC')
     config.set('PNC', 'restEndpoint', 'http://localhost:8080/pnc-rest/rest')
-    with open(os.path.join(configfile),'wb') as configfile:
+    with open(os.path.join(configfilename),'wb') as configfile:
         config.write(configfile)
 pnc_rest_url = config.get('PNC', 'restEndpoint')
 apiclient = swagger_client.api_client.ApiClient(pnc_rest_url)
 
 def get_api_client():
     return apiclient
+
+def gen_random_name():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
 
 def is_valid_version(version):
     pattern = re.compile('\d*\.\w*')
