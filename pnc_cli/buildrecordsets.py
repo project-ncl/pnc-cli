e6e1f5118351df4e477484402a0b16857f18e1fd
Tom Hauser <thauser@redhat.com>
2015-09-16 16:34:39 -0400

    prep buildrecordsets for implementation

--- pnc_cli/buildrecordsets.py
+++ pnc_cli/buildrecordsets.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 __author__ = 'thauser'
 import utils
 from swagger_client.apis import BuildrecordsetsApi
@@ -7,26 +5,50 @@ from swagger_client.apis import BuildrecordsetsApi
 brs_api = BuildrecordsetsApi(utils.get_api_client())
 
 def list_build_record_sets():
-    brs_api.get_all(callback=callback_function)
+    """
+    List all BuildRecordSets
+    """
+    response = utils.checked_api_call(brs_api,'get_all')
+    if response: return response.content
 
 def get_build_record_set(id):
-    brs_api.get_specific(id=id, callback=callback_function)
+    """
+    Get a specific BuildRecordSet by ID
+    """
+    response = utils.checked_api_call(brs_api,'get_specific',id=id)
+    if response: return response.content
 
 def create_build_record_set():
-    brs_api.create_new(callback=callback_function)
+    """
+    Create a new BuildRecordSet (incomplete)
+    """
+    response = utils.checked_api_call(brs_api, 'create_new')
+    if response: return response.content
 
 def list_sets_for_build_record(id):
-    brs_api.get_all_for_build_record(record_id=id,callback=callback_function)
+    """
+    List all BuildRecordSets of a given BuildRecord
+    """
+    response = utils.checked_api_call(brs_api, 'get_all_for_build_record', record_id=id)
+    if response: return response.content
 
 def list_build_record_sets_for_milestone(id):
-    brs_api.get_all_for_product_milestone(version_id=id, callback=callback_function)
+    """
+    List all BuildRecordSets for a given ProductMilestone
+    """
+    response = utils.checked_api_call(brs_api,'get_all_for_product_milestone', version_id=id)
+    if response: return response.content
 
 def delete_build_record_set(id):
-    brs_api.delete_specific(id=id, callback=callback_function)
+    """
+    Delete a specific BuildRecordSet by ID
+    """
+    response = utils.checked_api_call(brs_api, 'delete_specific', id=id)
+    if response: return response.content
 
 def update_build_record_set(id):
-    brs_api.update(id=id, callback=callback_function)
-
-def callback_function(response):
-    if response:
-        pprint(response.content)
+    """
+    Replace a specific BuildRecordSet with a new one.
+    """
+    response = utils.checked_api_call(brs_api, 'update', id=id)
+    if response: return response.content
