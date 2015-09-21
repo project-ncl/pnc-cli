651cffa9cb871a6a4c8eec8a4f691e8c74c44036
Tom Hauser <thauser@redhat.com>
2015-09-21 12:46:38 -0400

    mostly fix environment tests due to API changes

--- test/integration/test_environments_api.py
+++ test/integration/test_environments_api.py
@@ -4,8 +4,8 @@ from pnc_cli import utils
 
 envs_api = EnvironmentsApi(utils.get_api_client())
 
-def _create_env(bt="java", os="linux"):
-    return envs_api.create_new(body=environments._create_environment_object(build_type=bt.upper(),operational_system=os.upper())).content
+def _create_env(bt="java"):
+    return envs_api.create_new(body=environments._create_environment_object(build_type=bt.upper(),name=utils.gen_random_name())).content
 
 def test_get_all():
     envs = envs_api.get_all().content
@@ -13,26 +13,24 @@ def test_get_all():
 
 def test_create():
     new_env = _create_env()
-    envs = envs_api.get_all().content
-    env_ids = [env.id for env in envs]
+    env_ids = [env.id for env in envs_api.get_all().content]
     assert new_env.id in env_ids
 
 def test_get_specific():
     newenv = _create_env()
-    assert envs_api.get_specific(id=newenv.id) is not None
+    assert envs_api.get_specific(id=newenv.id).content is not None
 
 def test_delete():
     new_env = _create_env()
     envs_api.delete(new_env.id)
-    envs = envs_api.get_all().content
-    env_ids = [env.id for env in envs]
+    env_ids = [env.id for env in envs_api.get_all().content]
     assert new_env.id not in env_ids
 
 def test_update():
     new_env = _create_env()
-    updated_env = environments._create_environment_object(build_type="DOCKER",operational_system="WINDOWS")
+    updated_env = environments._create_environment_object(build_type="DOCKER",name="test-BuildEnvironmentName")
     envs_api.update(id=new_env.id, body=updated_env)
     retrieved_env = envs_api.get_specific(new_env.id).content
-    assert (retrieved_env.build_type == 'DOCKER') and (retrieved_env.operational_system == 'WINDOWS')
+    assert (retrieved_env.build_type == 'DOCKER') and (retrieved_env.name == 'test-BuildEnvironmentName')
 
 
