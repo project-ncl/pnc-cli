7059e8436dae2197d812edc91a123b558a357d01
Tom Hauser <thauser@redhat.com>
2015-11-02 17:02:13 -0500

    use fixtures to cleanup pnc instance after testing is complete

--- test/integration/test_buildconfigurationsets_api.py
+++ test/integration/test_buildconfigurationsets_api.py
@@ -13,10 +13,14 @@ from pnc_cli.swagger_client.apis.buildconfigurations_api import Buildconfigurati
 sets_api = BuildconfigurationsetsApi(utils.get_api_client())
 configs_api = BuildconfigurationsApi(utils.get_api_client())
 
-
-def create_set():
-    return sets_api.create_new(
+@pytest.fixture(scope='function')
+def new_set(request):
+    set = sets_api.create_new(
         body=buildconfigurationsets._create_build_config_set_object(name=testutils.gen_random_name(), productVersionId=1)).content
+    def teardown():
+        sets_api.delete_specific(id=set.id)
+    request.addfinalizer(teardown)
+    return set
 
 
 def test_get_all():
@@ -24,20 +28,17 @@ def test_get_all():
     assert sets is not None
 
 
-def test_create():
-    new_set = create_set()
+def test_create(new_set):
     set_names = [set.name for set in sets_api.get_all().content]
     assert new_set.name in set_names
 
 
-def test_get_specific():
-    new_set = create_set()
+def test_get_specific(new_set):
     set = sets_api.get_specific(id=new_set.id)
     assert set is not None
 
 
-def test_update():
-    new_set = create_set()
+def test_update(new_set):
     newname = "newname" + testutils.gen_random_name()
     updated_set = buildconfigurationsets._create_build_config_set_object(name=newname)
     sets_api.update(id=new_set.id, body=updated_set)
@@ -51,26 +52,24 @@ def test_delete():
 
 
 @pytest.mark.xfail(reason="doesn't reliably return valid json.")
-def test_trigger():
-    new_set = create_set()
+def test_trigger(new_set):
     running_set = sets_api.trigger(id=new_set.id)
     assert running_set.ok
 
 
-def test_get_configurations():
+def test_get_configurations(new_set):
     new_config_obj = buildconfigurations.create_build_conf_object(name=testutils.gen_random_name(), project_id="1",
                                                                   environment_id="1", build_status="SUCCESS")
     new_config = configs_api.create_new(body=new_config_obj).content
-    new_set = create_set()
-
     sets_api.add_configuration(id=new_set.id, body=new_config)
     set_configs = sets_api.get_configurations(id=new_set.id)
     assert set_configs is not None
 
 
-def test_add_configuration():
-    new_set = create_set()
-    new_config = test_buildconfigurations_api.new_config()
+def test_add_configuration(new_set):
+    new_config_obj = buildconfigurations.create_build_conf_object(name=testutils.gen_random_name(), project_id="1",
+                                                                  environment_id="1", build_status="SUCCESS")
+    new_config = configs_api.create_new(body=new_config_obj).content
     sets_api.add_configuration(id=new_set.id, body=new_config)
     set_config_ids = [x.id for x in sets_api.get_configurations(id=new_set.id).content]
     assert new_config.id in set_config_ids
