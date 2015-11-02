42fa2905ed36016c9977e02a092d3652da3c980c
Tom Hauser <thauser@redhat.com>
2015-11-02 17:07:42 -0500

    convert licenses integration test to use fixtures for cleanup of pnc instance after testing

--- test/integration/test_licenses_api.py
+++ test/integration/test_licenses_api.py
@@ -1,3 +1,4 @@
+import pytest
 from pnc_cli import licenses
 from pnc_cli.swagger_client.apis.licenses_api import LicensesApi
 from pnc_cli import utils
@@ -6,39 +7,38 @@ from test import testutils
 licenses_api = LicensesApi(utils.get_api_client())
 
 
-def _create_license():
-    return licenses_api.create_new(body=licenses._create_license_object(full_name=testutils.gen_random_name(),
+@pytest.fixture(scope='function')
+def new_license(request):
+    license = licenses_api.create_new(body=licenses._create_license_object(full_name=testutils.gen_random_name(),
                                                                         full_content="pnc_cli-cli test license")).content
+    def teardown():
+        existing_licenses = licenses_api.get_all().content
+        if existing_licenses and license.id in [x.id for x in existing_licenses]:
+            licenses_api.delete(id=license.id)
+    request.addfinalizer(teardown)
+    return license
 
-
-def test_get_all():
+def test_get_all(new_license):
     # empty at first. create one to test.
-    _create_license()
     l = licenses_api.get_all().content
     assert l is not None
 
 
-def test_create_license():
-    new_license = _create_license()
+def test_create_license(new_license):
     license_ids = [x.id for x in licenses_api.get_all().content]
     assert new_license.id in license_ids
 
 
-def test_get_specific():
-    new_license = _create_license()
+def test_get_specific(new_license):
     assert licenses_api.get_specific(new_license.id) is not None
 
 
-def test_delete():
-    new_license = _create_license()
+def test_delete(new_license):
     licenses_api.delete(new_license.id)
-    l_ids = [x.id for x in licenses_api.get_all().content]
-    assert new_license.id not in l_ids
+    assert not licenses_api.get_all().content
 
 
-def test_update():
-    new_license = _create_license()
-    licenses_api.update(id=new_license.id, body=licenses._create_license_object(full_name='PNC-CLI updated license',
-                                                                                full_content='updated content'))
+def test_update(new_license):
+    licenses_api.update(id=new_license.id, body=licenses._create_license_object(full_name='PNC-CLI updated license',                                                                                full_content='updated content'))
     updated_license = licenses_api.get_specific(new_license.id).content
     assert updated_license.full_name == 'PNC-CLI updated license' and updated_license.full_content == 'updated content'
