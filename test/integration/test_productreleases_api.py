07f5eef3a9e88bba05a6348aa7d2a208f5789d6a
Tom Hauser <thauser@redhat.com>
2015-11-04 15:27:26 -0500

    add fixtures for creating product version / milestone / release for testing. only test failure is update, which modifies release date for unknown reason

--- test/integration/test_productreleases_api.py
+++ test/integration/test_productreleases_api.py
@@ -1,23 +1,53 @@
 __author__ = 'thauser'
 import pytest
 from pnc_cli.swagger_client.apis import ProductreleasesApi
+from pnc_cli.swagger_client.apis import ProductmilestonesApi
+from pnc_cli.swagger_client.apis import ProductversionsApi
 from pnc_cli import utils
 from pnc_cli import productreleases
+from pnc_cli import productmilestones
+from pnc_cli import productversions
 from test import testutils
 
+
+milestones_api = ProductmilestonesApi(utils.get_api_client())
 releases_api = ProductreleasesApi(utils.get_api_client())
+versions_api = ProductversionsApi(utils.get_api_client())
 
 
 @pytest.fixture(scope='function')
-def new_release():
+def new_version():
+    version = versions_api.create_new_product_version(body=productversions.create_product_version_object(
+        version=testutils.gen_random_version(),
+        product_id=1,
+        current_product_milestone_id=1
+    )).content
+    return version
+
+
+@pytest.fixture(scope='function')
+def new_milestone(new_version):
+    milestone = milestones_api.create_new(body=productmilestones.create_milestone_object(
+        version=new_version.version+'.1.build3',
+        starting_date='2016-01-01',
+        planned_end_date='2017-01-01',
+        download_url='localhost:8080/build3',
+        product_version_id=new_version.id
+    )).content
+    return milestone
+
+
+@pytest.fixture(scope='function')
+def new_release(new_milestone):
+    associated_version = versions_api.get_specific(id=new_milestone.product_version_id).content.version
     release = releases_api.create_new(body=productreleases.create_product_release_object(
-        version=testutils.gen_random_version() + ".9.DR1",
+        version= associated_version + ".1.DR1",
         release_date="2016-01-01",
         download_url="pnc-cli-test-url",
-        product_version_id=1,
-        product_milestone_id=1,
+        product_version_id=new_milestone.product_version_id,
+        product_milestone_id=new_milestone.id,
         support_level='EOL'
-    ))
+    )).content
     return release
 
 
@@ -68,7 +98,7 @@ def test_get_specific_invalid_param():
 
 
 def test_get_specific(new_release):
-    retrieved_release = releases_api.get_specific(id=new_release.id)
+    retrieved_release = releases_api.get_specific(id=new_release.id).content
     assert retrieved_release.to_dict() == new_release.to_dict()
 
 
@@ -83,5 +113,5 @@ def test_update_invalid_param():
 def test_update(new_release):
     new_release.download_url = 'updated-download-url'
     releases_api.update(id=new_release.id, body=new_release)
-    updated = releases_api.get_specific(id=new_release.id)
+    updated = releases_api.get_specific(id=new_release.id).content
     assert updated.to_dict() == new_release.to_dict()
