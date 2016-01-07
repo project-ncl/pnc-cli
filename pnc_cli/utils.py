05a1de4e88d0c1ddf7ff7191049c0be5a0f0e112
Tom Hauser <thauser@redhat.com>
2016-01-07 14:51:42 -0500

    tentative restructure of the authentication code to improve performance drastically

--- pnc_cli/utils.py
+++ pnc_cli/utils.py
@@ -63,7 +63,8 @@ def get_auth_token(config):
     r = requests.post(server, params, verify=False)
     if r.status_code == 200:
         reply = json.loads(r.content.decode('utf-8'))
-        return str(reply.get('access_token'))
+        global authtoken
+        authtoken = str(reply.get('access_token'))
 
 
 config = configparser.ConfigParser(allow_no_value=False)
@@ -91,11 +92,11 @@ if not found:
     logging.warning(
         "New config file written to ~/.config/pnc-cli/pnc-cli.conf. Configure pncUrl and keycloakUrl values.")
     exit(0)
-
+authtoken = get_auth_token(config)
 
 def get_api_client():
     pnc_rest_url = config.get('PNC', 'pncUrl').rstrip('/') + '/pnc-rest/rest'
-    authtoken = get_auth_token(config)
+    global authtoken
     if authtoken:
         apiclient = swagger_client.ApiClient(pnc_rest_url, header_name='Authorization',
                                              header_value="Bearer " + authtoken)
