a7831bf2a6d981bb2e22de6082abd838cf9898ee
Tom Hauser <thauser@redhat.com>
2016-02-05 16:20:21 -0500

    tweak some tests

--- test/integration/test_buildtasks_api.py
+++ test/integration/test_buildtasks_api.py
@@ -12,19 +12,19 @@ def get_tasks_api():
 
 
 def test_build_task_completed_no_task_id():
-    testutils.assert_raises_valueerror(tasks_api, 'build_task_completed', task_id=None, build_status='NEW')
+    testutils.assert_raises_valueerror(tasks_api, 'build_task_completed', task_id=None, build_result='NEW')
 
 
-def test_build_task_completed_no_build_status():
-    testutils.assert_raises_valueerror(tasks_api, 'build_task_completed', task_id=1, build_status=None)
+def test_build_task_completed_no_build_result():
+    testutils.assert_raises_valueerror(tasks_api, 'build_task_completed', task_id=1, build_result=None)
 
 
 def test_build_task_completed_invalid_param():
-    testutils.assert_raises_typeerror(tasks_api, 'build_task_completed', task_id=1, build_status='NEW')
+    testutils.assert_raises_typeerror(tasks_api, 'build_task_completed', task_id=1, build_result='NEW')
 
 
 @pytest.mark.xfail(reason='unsure of how to test this correctly.')
 def test_build_task_completed():
-    response = tasks_api.build_task_completed(task_id=1, build_status='NEW')
+    response = tasks_api.build_task_completed(task_id=1, build_result='NEW')
     assert response
 
