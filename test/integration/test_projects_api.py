7dfb6bd5c02ddaead6b3dd9c22cef09a4fdcc8ad
Tom Hauser <thauser@redhat.com>
2016-05-03 14:21:54 -0400

    project creation fixture to 'module' scope, add finalizer to remove when finished

--- test/integration/test_projects_api.py
+++ test/integration/test_projects_api.py
@@ -10,9 +10,12 @@ def get_projects_api():
     projects_api = ProjectsApi(utils.get_api_client())
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='module')
 def new_project(request):
     project = projects_api.create_new(body=projects._create_project_object(name=testutils.gen_random_name())).content
+    def teardown():
+        projects.delete_project(id=project.id)
+    request.addfinalizer(teardown)
     return project
 
 
