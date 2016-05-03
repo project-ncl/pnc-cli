ca04cdf1c7192a047c0ed9fbb3aaf1f043d306df
Tom Hauser <thauser@redhat.com>
2016-05-03 16:41:25 -0400

    update test_coverage_report script to ignore irrelevant files

--- test_coverage_report.sh
+++ test_coverage_report.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 coverage run runtests.py -vv
-coverage report -m --omit=test/*,*swagger_client/models/*,runtests*,/usr/*,*test_api*
+coverage report -m --omit=test/*,*swagger_client/models/*,runtests*,/usr/*,*test_api*,*swagger_client/configuration.py,*swagger_client/rest.py,*__init__.py*,pnc_cli/utils.py,*api_client.py*
