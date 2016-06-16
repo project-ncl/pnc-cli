6e79c0b7405aa712603fcc73bc50ee38ae98f3ab
Daniel Soukhov <dsoukhov@unused-10-15-17-220.yyz.redhat.com>
2016-06-16 15:08:25 -0400

    probably cause a conflict

--- pnc_cli/types.py
+++ pnc_cli/types.py
@@ -11,6 +11,7 @@ from pnc_cli.swagger_client import EnvironmentsApi
 from pnc_cli.swagger_client import ProductsApi
 from pnc_cli.swagger_client import ProductversionsApi
 from pnc_cli.swagger_client import ProjectsApi
+from pnc_cli.swagger_client import LicensesApi
 
 api_client = utils.get_api_client()
 configs_api = BuildconfigurationsApi(api_client)
@@ -21,6 +22,7 @@ projects_api = ProjectsApi(api_client)
 versions_api = ProductversionsApi(api_client)
 bcsr_api = BuildconfigsetrecordsApi(api_client)
 records_api = BuildrecordsApi(api_client)
+license_api = LicensesApi(api_client)
 
 bc_name_regex = "^[a-zA-Z0-9_.][a-zA-Z0-9_.-]*(?!\.git)+$"
 
@@ -141,3 +143,17 @@ def existing_build_record(id_input):
     if not common.id_exists(records_api, id_input):
         raise argparse.ArgumentTypeError("no BuildRecord with ID {} exists".format(id_input))
     return id_input
+
+
+def existing_license_id(license_id):
+    utils.valid_id(license_id)
+    if not common.id_exists(license_api, license_id):
+        raise argparse.ArgumentTypeError("No license with id {} exists".format(license_id))
+    return license_id
+
+
+def existing_license_name(name):
+    utils.valid_id(name)
+    if not common.id_exists(license_api, name):
+        raise argparse.ArgumentTypeError("No license with name {} exists".format(name))
+    return name
