8435581b3e1052fb6be8626794b2af6bcbb119ec
Pavel Kralik <pkralik@redhat.com>
2016-10-06 16:34:41 +0200

    BPM Create Configuration

---
+++ pnc_cli/bpmbuildconfigurations.py
@@ -0,0 +1,53 @@
+from argh import arg
+
+import pnc_cli.common as common
+import pnc_cli.cli_types as types
+from pnc_cli import swagger_client
+from pnc_cli import utils
+from pnc_cli.swagger_client import BpmApi
+from pnc_cli.swagger_client import EnvironmentsApi
+from pnc_cli.swagger_client import ProjectsApi
+
+api_client = utils.get_api_client()
+projects_api = ProjectsApi(api_client)
+configs_api = BpmApi(api_client)
+envs_api = EnvironmentsApi(api_client)
+
+def create_build_conf_object(**kwargs):
+    created_build_configuration = swagger_client.BpmBuildConfigurationCreationRest()
+    for key, value in kwargs.items():
+        setattr(created_build_configuration, str(key), value)
+    return created_build_configuration
+
+@arg("name", help="Name for the new BuildConfiguration.", type=types.valid_bc_name)
+# allow specifying project by name?
+@arg("project", help="ID of the Project to associate the BuildConfiguration with.", type=types.existing_project_id)
+@arg("environment", help="ID of the Environment for the new BuildConfiguration.",
+     type=types.existing_environment_id)
+@arg("scm_repo_url", help="URL to the sources of the BuildConfiguration.")
+@arg("scm_revision", help="Revision of the sources in scm-url for this BuildConfiguration.")
+@arg("scm_external_repo_url", help="URL to the external sources of the BuildConfiguration.")
+@arg("scm_external_revision", help="Revision of the external sources in scm-url for this BuildConfiguration.")
+@arg("build_script", help="Script to execute for the BuildConfiguration.")
+@arg("-d", "--description", help="Description of the new build configuration.")
+@arg("-pvi", "--product-version-id", help="Associated ProductVersion ID.")
+@arg("-dids", "--dependency-ids", type=int, nargs="+",
+     help="List of BuildConfiguration IDs that are dependencies of this BuildConfiguration.")
+# @arg("-bcsid", "--")
+def create_bpm_build_configuration(**kwargs):
+    """
+    Create a new BuildConfiguration. BuildConfigurations represent the settings and configuration required to run a build of a specific version of the associated Project's source code.
+    If a ProductVersion ID is provided, the BuildConfiguration will have access to artifacts which were produced for that version, but may not have been released yet.
+    """
+    project_id = kwargs.get('project')
+    project_rest = common.get_entity(projects_api, project_id)
+    kwargs['project'] = project_rest
+    env_id = kwargs.get('environment')
+    env_rest = common.get_entity(envs_api, env_id)
+    kwargs['environment'] = env_rest
+
+    build_configuration = create_build_conf_object(**kwargs)
+    response = utils.checked_api_call(
+        configs_api, 'start_bc_creation_task', body=build_configuration)
+    if response:
+        return response.content
