fd8a15e2a9e122a1c74a976c7f9373cfc5fe4ec5
Pavel Kralik <pkralik@redhat.com>
2016-10-07 15:11:53 +0200

    Added EAP7 config test

--- test/product/test_products.py
+++ test/product/test_products.py
@@ -126,6 +126,25 @@ def test_jdg_infinispan830ga():
     assert build_record is not None
     print set.id
 
+def test_eap701ga():
+    # EAP 7.0.1.GA
+    sufix = get_sufix()
+    project = projects.get_project(name="eap7")
+    eap_name = "org.jboss.eap-jboss-eap-parent-7.0.1.GA-redhat-2" + sufix
+    build_config = buildconfigurations.create_build_configuration(
+                                                                  name=eap_name,
+                                                                  project=project.id,
+                                                                  environment=1,
+                                                                  scm_repo_url="git+ssh://user-pnc-gerrit@pnc-gerrit.pnc.dev.eng.bos.redhat.com:29418/pnc/org.jboss.eap-jboss-eap-parent-7.0.1.GA-redhat-2.git",
+                                                                  scm_revision="branch-7.0.1.GA-redhat-2-pnc",
+                                                                  build_script="mvn clean deploy -Prelease")
+
+    set = buildconfigurationsets.create_build_configuration_set(name=eap_name)
+    buildconfigurationsets.add_build_configuration_to_set(set_id=set.id, config_id=build_config.id)
+    build_record = buildconfigurationsets.build_set(id=set.id)
+    assert build_record is not None
+    print set.id
+
 def get_sufix():
     return "-" + ''.join(random.choice(string.ascii_uppercase + string.digits)
                          for _ in range(10))
