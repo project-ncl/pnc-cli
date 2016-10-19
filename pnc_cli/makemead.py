f215b5e4f43d2d952050f911d456073bdcd93a39
Pavel Kralik <pkralik@redhat.com>
2016-10-19 14:53:49 +0200

    Build Group Config name change

--- pnc_cli/makemead.py
+++ pnc_cli/makemead.py
@@ -49,11 +49,14 @@ def make_mead(config="cfg/builder.cfg", artifact=None):
         package = art_params['package']
         version = art_params['version']
         scm_url = art_params['scmURL']
+        target = art_params['target']
         (scm_repo_url, scm_revision) = scm_url.split("#", 2)
         artifact_name = package + "-" + version + sufix
+        target_name = target + sufix
         if set is None:
-            set = buildconfigurationsets.create_build_configuration_set(name=artifact_name)
-            print set.id 
+            set = buildconfigurationsets.create_build_configuration_set(name=target_name)
+            print target_name + ":"
+            print set.id
         try:
             project = projects.get_project(name=artifact)        
         except ValueError:
@@ -67,9 +70,10 @@ def make_mead(config="cfg/builder.cfg", artifact=None):
                                                                       scm_revision=scm_revision,
                                                                       build_script="mvn clean deploy -DskipTests" + get_maven_options(art_params))
         buildconfigurationsets.add_build_configuration_to_set(set_id=set.id, config_id=build_config.id)
+        print artifact + ":"
         print build_config.id
     build_record = buildconfigurationsets.build_set(id=set.id)
-    pprint(build_record)
+    #pprint(build_record)
 
     return config
 
