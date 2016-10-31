96fce69e501d45a6c75e52b5d75d9c852c76b50b
Pavel Kralik <pkralik@redhat.com>
2016-10-31 11:47:55 +0100

    Leaving the latest EAP build

--- test/product/test_products.py
+++ test/product/test_products.py
@@ -5,26 +5,6 @@ from pnc_cli import projects
 import random
 import string
 
-def test_eap():
-    # EAP 7.0.3.GA
-    sufix = get_sufix()
-    set_name = "jb-eap-7.0-rhel-7-candidate" + sufix
-    set = buildconfigurationsets.create_build_configuration_set(name=set_name)
-    project = projects.get_project(name="eap7")
-    eap_name = "org.jboss.eap-jboss-eap-parent-7.0.3.GA-redhat-2" + sufix
-    build_config = buildconfigurations.create_build_configuration(
-                                                                  name=eap_name,
-                                                                  project=project.id,
-                                                                  environment=1,
-                                                                  scm_repo_url="git+ssh://user-pnc-gerrit@pnc-gerrit.pnc.dev.eng.bos.redhat.com:29418/pnc/org.jboss.eap-jboss-eap-parent-7.0.3.GA-redhat-2.git",
-                                                                  scm_revision="branch-7.0.3.GA-redhat-2-pnc",
-                                                                  build_script="mvn clean deploy -DskipTests -Prelease")
-
-    buildconfigurationsets.add_build_configuration_to_set(set_id=set.id, config_id=build_config.id)
-    build_record = buildconfigurationsets.build_set(id=set.id)
-    assert build_record is not None
-    print set.id
-
 def test_eap71():
     # EAP 7.1.0.Alpha
     sufix = get_sufix()
