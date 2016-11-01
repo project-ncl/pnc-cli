49f1576eead46d9f9426168a4b8345357490222e
Pavel Kralik <pkralik@redhat.com>
2016-11-01 17:22:19 +0100

    EAP test build done via CFG file

--- test/product/test_products.py
+++ test/product/test_products.py
@@ -1,35 +1,9 @@
-from pnc_cli import buildconfigurations
-from pnc_cli import buildconfigurationsets
 from pnc_cli import makemead
-from pnc_cli import projects
-import logging
 import random
 import string
 
 def test_eap():
-    # EAP 7.1.0.Alpha
-    project_name = "eap7"
-    sufix = get_sufix()
-    set_name = "jb-eap-7.1-rhel-7-candidate" + sufix
-    set = buildconfigurationsets.create_build_configuration_set(name=set_name)
-    try:
-        project = projects.get_project(name=project_name)
-    except ValueError:
-        logging.error('No project ' + project_name)
-        project = projects.create_project(name=project_name)
-    eap_name = "org.jboss.eap-jboss-eap-parent-7.1.0.Alpha1-redhat-8" + sufix
-    build_config = buildconfigurations.create_build_configuration(
-                                                                  name=eap_name,
-                                                                  project=project.id,
-                                                                  environment=1,
-                                                                  scm_repo_url="git+ssh://user-pnc-gerrit@pnc-gerrit.pnc.dev.eng.bos.redhat.com:29418/pnc/org.jboss.eap-jboss-eap-parent-7.1.0.Alpha1-redhat-8.git",
-                                                                  scm_revision="branch-EAP_7.1.0.DR7",
-                                                                  build_script="mvn clean deploy -DskipTests=true -Prelease")
-
-    buildconfigurationsets.add_build_configuration_to_set(set_id=set.id, config_id=build_config.id)
-    build_record = buildconfigurationsets.build_set(id=set.id)
-    assert build_record is not None
-    print set.id
+    makemead.make_mead(config="cfg/eap.cfg", cproject=True)
 
 def test_sso():
     makemead.make_mead(config="cfg/sso.cfg", cproject=True)
