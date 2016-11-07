a286b5c90ff9092c1c3447b09ca0674bbc748a43
Pavel Kralik <pkralik@redhat.com>
2016-11-07 17:12:42 +0100

    Update functionality added

--- pnc_cli/makemead.py
+++ pnc_cli/makemead.py
@@ -7,19 +7,22 @@ from pnc_cli import buildconfigurations
 from pnc_cli import buildconfigurationsets
 from pnc_cli import projects
 from pprint import pprint
-import random
-import string
 from tools.config_utils import ConfigReader
 
 @arg('-c', '--config', help='Configuration file to use to drive the build')
-@arg('-p', '--cproject', help='Create project if needed')
+@arg('-b', '--run_build', help='Run Build')
+@arg('-e', '--environment', help='Environment ID (default 1)')
 @arg('-s', '--sufix', help='Adding sufix to artifact\'s name')
-def make_mead(config="cfg/builder.cfg", cproject=None, sufix=""):
+def make_mead(config=None, run_build=None, environment=1, sufix=""):
     """
     Create Make Mead configuration
     :param config: Make Mead config name
     :return:
     """    
+    if config is None:
+        logging.error('Config file --config is not specified.')
+        return 1
+
     if not os.path.isfile(config):
         logging.error('Config file %s not found.', os.path.abspath(config))
         return 1
@@ -53,28 +56,45 @@ def make_mead(config="cfg/builder.cfg", cproject=None, sufix=""):
         (scm_repo_url, scm_revision) = scm_url.split("#", 2)
         artifact_name = package + "-" + version + sufix
         target_name = target + sufix
+
         if set is None:
-            set = buildconfigurationsets.create_build_configuration_set(name=target_name)
+            try:
+                set = buildconfigurationsets.get_build_configuration_set(name=target_name)
+            except ValueError:
+                set = buildconfigurationsets.create_build_configuration_set(name=target_name)
             logging.debug(target_name + ":")
             logging.debug(set.id)
+
         try:
             project = projects.get_project(name=artifact)        
         except ValueError:
-            logging.error('No project ' + artifact)
-            if cproject is None:
-                return 1
-            else:
-                project = projects.create_project(name=artifact)
-        build_config = buildconfigurations.create_build_configuration(
+            logging.debug('No project ' + artifact)
+            project = projects.create_project(name=artifact)
+        logging.debug(artifact_name + ":")
+        logging.debug(project.id)
+
+        try:
+            build_config_id = buildconfigurations.get_build_configuration_id_by_name(name=artifact_name)
+            buildconfigurations.update_build_configuration(
+                                                                      id=build_config_id,
                                                                       name=artifact_name,
                                                                       project=project.id,
-                                                                      environment=1, 
+                                                                      environment=environment, 
                                                                       scm_repo_url=scm_repo_url,
                                                                       scm_revision=scm_revision,
                                                                       build_script="mvn clean deploy -DskipTests" + get_maven_options(art_params))
-        buildconfigurationsets.add_build_configuration_to_set(set_id=set.id, config_id=build_config.id)
+            build_config = buildconfigurations.get_build_configuration(id=build_config_id)
+        except ValueError:
+            logging.debug('No build config with name ' + artifact_name)
+            build_config = buildconfigurations.create_build_configuration(
+                                                                      name=artifact_name,
+                                                                      project=project.id,
+                                                                      environment=environment, 
+                                                                      scm_repo_url=scm_repo_url,
+                                                                      scm_revision=scm_revision,
+                                                                      build_script="mvn clean deploy -DskipTests" + get_maven_options(art_params))
+            buildconfigurationsets.add_build_configuration_to_set(set_id=set.id, config_id=build_config.id)
         ids[artifact] = build_config
-        logging.debug(artifact_name + ":")
         logging.debug(build_config.id)
     logging.debug(ids)
     for package, dependencies in packages.iteritems():
@@ -83,9 +103,12 @@ def make_mead(config="cfg/builder.cfg", cproject=None, sufix=""):
             subid = ids[artifact]
             logging.debug(id.id, subid.id)
             buildconfigurations.add_dependency(id=id.id, dependency_id=subid.id)
-    build_record = buildconfigurationsets.build_set(id=set.id)
 
-    return build_record
+    if run_build is not None:
+        build_record = buildconfigurationsets.build_set(id=set.id)
+        pprint(build_record)
+
+    return set
 
 def get_maven_options(params):
     result = ""
