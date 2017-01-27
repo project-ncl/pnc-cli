dc05c34db65ecc4830acd752057d7b2ba0cd0eeb
Tom Hauser <thauser@redhat.com>
2017-01-27 17:25:47 -0500

    remove configuration related parsing stuff

--- pnc_cli/utils.py
+++ pnc_cli/utils.py
@@ -1,8 +1,8 @@
 import getpass
-import json
 import logging
 
 import requests
+
 
 requests.packages.urllib3.disable_warnings()
 
@@ -21,7 +21,6 @@ try:
 except NameError:
     pass
 
-from pnc_cli import swagger_client
 from pnc_cli.swagger_client.rest import ApiException
 
 __author__ = 'thauser'
@@ -29,87 +28,6 @@ __author__ = 'thauser'
 CONFIG_LOCATION = os.path.expanduser("~") + "/.config/pnc-cli/"
 CONFIG_FILENAME = "pnc-cli.conf"
 CONFIG = CONFIG_LOCATION + CONFIG_FILENAME
-
-def get_auth_token(config):
-    try:
-        server = config.get('PNC', 'keycloakUrl')
-        if server is None:
-            raise configparser.NoOptionError
-    except configparser.NoOptionError:
-        logging.error(
-            'No authentication server defined. Define "keycloakUrl" in pnc-cli.conf to enable authentication.')
-        return
-    try:
-        realm = config.get('PNC', 'keycloakRealm')
-    except configparser.NoOptionError:
-        logging.error(
-            'No keycloak authentication realm defined. Define "keycloakRealm" in pnc-cli.conf to enable authentication.')
-        return
-    server = server + "/auth/realms/" + realm + "/protocol/openid-connect/token"
-    try:
-        username = config.get('PNC', 'username')
-        password = config.get('PNC', 'password')
-    except configparser.NoOptionError:
-        logging.error(
-            'Username / password missing. Define "username" and "password" in pnc-cli.conf for authentication.')
-        return
-
-    try:
-        client_id = config.get('PNC', 'keycloakClientId')
-    except configparser.NoOptionError:
-        logging.error(
-            'client_id is missing for the keycloak payload. Define "keycloakClientId" in pnc-cli.conf for authentication.')
-        return
-
-    params = {'grant_type': 'password',
-              'client_id': client_id,
-              'username': username,
-              'password': password}
-    r = requests.post(server, params, verify=False)
-    if r.status_code == 200:
-        reply = json.loads(r.content.decode('utf-8'))
-        return str(reply.get('access_token'))
-
-
-config = configparser.ConfigParser(allow_no_value=False)
-configfilename = os.path.expanduser("~") + "/.config/pnc-cli/pnc-cli.conf"
-configdirname = os.path.dirname(configfilename)
-
-try:
-    os.makedirs(configdirname)
-except OSError as e:
-    if e.errno != errno.EEXIST:
-        raise
-found = config.read(os.path.join(configfilename))
-if not found:
-    config.add_section('PNC')
-    config.set('PNC', 'pncUrl', 'http://localhost:8080/')
-    # prompt user for his keycloak username / passwords
-    config.set('PNC', 'keycloakUrl', '')
-    config.set('PNC', 'keycloakRealm', 'pncredhat')
-    config.set('PNC', 'keycloakClientId', 'pncdirect')
-    username = input('Username: ')
-    password = getpass.getpass('Password: ')
-    config.set('PNC', 'username', username)
-    config.set('PNC', 'password', password)
-    with open(os.path.join(configfilename), 'w') as configfile:
-        config.write(configfile)
-    logging.warning(
-        "New config file written to ~/.config/pnc-cli/pnc-cli.conf. Configure pncUrl and keycloakUrl values.")
-    exit(0)
-global authtoken
-authtoken = get_auth_token(config)
-
-
-def get_api_client():
-    pnc_rest_url = config.get('PNC', 'pncUrl').rstrip('/') + '/pnc-rest/rest'
-    if authtoken:
-        apiclient = swagger_client.ApiClient(pnc_rest_url, header_name='Authorization',
-                                             header_value="Bearer " + authtoken)
-    else:
-        logging.warning('Authentication failed. Some operations will be unavailable.')
-        apiclient = swagger_client.ApiClient(pnc_rest_url)
-    return apiclient
 
 
 def is_valid_version(version, regex):
