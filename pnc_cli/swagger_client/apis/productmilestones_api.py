ecdb6c3bc4c92597009983f8764865eba9b477bc
Dustin Kut Moy Cheung <dcheung@redhat.com>
2017-10-13 16:36:43 -0400

    [NCL-3362] Swagger API regen for close milestone

--- pnc_cli/swagger_client/apis/productmilestones_api.py
+++ pnc_cli/swagger_client/apis/productmilestones_api.py
@@ -126,6 +126,87 @@ class ProductmilestonesApi(object):
                                             callback=params.get('callback'))
         return response
 
+    def close_milestone(self, id, **kwargs):
+        """
+        Close/Release a Product Milestone
+        
+
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please define a `callback` function
+        to be invoked when receiving the response.
+        >>> def callback_function(response):
+        >>>     pprint(response)
+        >>>
+        >>> thread = api.close_milestone(id, callback=callback_function)
+
+        :param callback function: The callback function
+            for asynchronous request. (optional)
+        :param int id: Product Milestone id (required)
+        :param ProductMilestoneRest body: 
+        :return: None
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        # verify the required parameter 'id' is set
+        if id is None:
+            raise ValueError("Missing the required parameter `id` when calling `close_milestone`")
+
+        all_params = ['id', 'body']
+        all_params.append('callback')
+
+        params = locals()
+        for key, val in iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method close_milestone" % key
+                )
+            params[key] = val
+        del params['kwargs']
+
+        resource_path = '/product-milestones/{id}/close-milestone'.replace('{format}', 'json')
+        method = 'PUT'
+
+        path_params = {}
+        if 'id' in params:
+            path_params['id'] = params['id']
+
+        query_params = {}
+
+        header_params = {}
+
+        form_params = {}
+        files = {}
+
+        body_params = None
+        if 'body' in params:
+            body_params = params['body']
+
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.\
+            select_header_accept(['application/json'])
+        if not header_params['Accept']:
+            del header_params['Accept']
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.\
+            select_header_content_type(['application/json'])
+
+        # Authentication setting
+        auth_settings = []
+
+        response = self.api_client.call_api(resource_path, method,
+                                            path_params,
+                                            query_params,
+                                            header_params,
+                                            body=body_params,
+                                            post_params=form_params,
+                                            files=files,
+                                            response_type=None,
+                                            auth_settings=auth_settings,
+                                            callback=params.get('callback'))
+        return response
+
     def create_new(self, **kwargs):
         """
         Creates a new Product Milestone for the Specified Product Version
