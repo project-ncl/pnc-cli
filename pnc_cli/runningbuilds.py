d53f6f045aa506ae8f66bb13340a09e3f4d11851
Honza Brázdil <jbrazdil@redhat.com>
2018-04-12 18:00:45 +0200

    Fix runningbuilds tests

--- pnc_cli/runningbuilds.py
+++ pnc_cli/runningbuilds.py
@@ -17,9 +17,15 @@ def list_running_builds(page_size=200, page_index=0, sort=""):
     """
     List all RunningBuilds
     """
+    content = list_running_builds_raw(page_size, page_index, sort)
+    if content:
+        return utils.format_json_list(content)
+
+
+def list_running_builds_raw(page_size=200, page_index=0, sort=""):
     response = utils.checked_api_call(running_api, 'get_all', page_size=page_size, page_index=page_index, sort=sort)
     if response:
-        return utils.format_json_list(response.content)
+        return response.content
 
 
 @arg("id", help="ID of the RunningBuild to retrieve.", type=cli_types.existing_running_build)
@@ -27,6 +33,12 @@ def get_running_build(id):
     """
     Get info about a specific RunningBuild
     """
+    content = get_running_build_raw(id)
+    if content:
+        return utils.format_json(content)
+
+
+def get_running_build_raw(id):
     response = utils.checked_api_call(running_api, 'get_specific', id=id)
     if response:
-        return utils.format_json(response.content)
+        return response.content
--- test/unit/test_runningbuilds.py
+++ test/unit/test_runningbuilds.py
@@ -6,13 +6,13 @@ __author__ = 'thauser'
 
 @patch('pnc_cli.runningbuilds.running_api.get_all', return_value=MagicMock(content=[1, 2, 3]))
 def test_list_running_builds(mock):
-    result = runningbuilds.list_running_builds()
-    mock.assert_called_once_with(page_size=200, sort="")
+    result = runningbuilds.list_running_builds_raw()
+    mock.assert_called_once_with(page_index=0, page_size=200, sort="")
     assert result == [1, 2, 3]
 
 
 @patch('pnc_cli.runningbuilds.running_api.get_specific', return_value=MagicMock(content='running-build'))
 def test_get_running_build(mock):
-    result = runningbuilds.get_running_build(1)
+    result = runningbuilds.get_running_build_raw(1)
     mock.assert_called_once_with(id=1)
     assert result == 'running-build'
