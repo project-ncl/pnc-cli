fb2b711de72d0fd52183fe666daa992aa9bfc7d9
Honza Brázdil <jbrazdil@redhat.com>
2018-04-12 16:32:53 +0200

    Fix environments tests and remove commands to removed endpoints

--- pnc_cli/environments.py
+++ pnc_cli/environments.py
@@ -15,96 +15,20 @@ envs_api = EnvironmentsApi(uc.user.get_api_client())
 __author__ = 'thauser'
 
 
-def create_environment_object(**kwargs):
-    created_environment = BuildEnvironmentRest()
-    for key, value in iteritems(kwargs):
-        if value:
-            setattr(created_environment, key, value)
-    return created_environment
-
-
-def valid_attribute(attributeInput):
-    if attributeInput.count("=") == 0:
-        raise argparse.ArgumentTypeError("Invalid attribute syntax. Correct syntax: key=value")
-    attribute_key, attribute_value = attributeInput.split('=', 1)
-    return {attribute_key: attribute_value}
-
-
-def unique_iid(iidInput):
-    response = utils.checked_api_call(envs_api, 'get_all', q='systemImageId==' + iidInput)
-    if response.content:
-        raise argparse.ArgumentTypeError("systemImageId is already in use")
-    return iidInput
-
-
-@arg("name", help="Unique name of the BuildEnvironment", type=types.unique_environment_name)
-@arg("system_image_id", help="ID of the Docker image for this BuildEnvironment.", type=unique_iid)
-@arg("system_image_type", help="One of DOCKER_IMAGE, VIRTUAL_MACHINE_RAW, VIRTUAL_MACHINE_QCOW2, LOCAL_WORKSPACE",
-     choices=["DOCKER_IMAGE", "VIRTUAL_MACHINE_RAW", "VIRTUAL_MACHINE_QCOW2", "LOCAL_WORKSPACE"])
-@arg("-d", "--description", help="Description of the BuildEnvironment.")
-@arg("-a", "--attributes", help="Attributes of the BuildEnvironment. Syntax: Key=Value", type=valid_attribute)
-@arg("-iru", "--image-repository-url", help="URL for the Docker repository in which the image resides.")
-def create_environment(**kwargs):
-    """
-    Create a new Environment
-    """
-    environment = create_environment_object(**kwargs)
-    response = utils.checked_api_call(envs_api, 'create_new', body=environment)
-    if response:
-        return utils.format_json(response.content)
-
-
-@arg("id", help="ID of the environment to update.", type=types.existing_environment_id)
-@arg("-s", "--system-image-type", help="Updated system image type for the new BuildEnvironment.")
-@arg("-d", "--description", help="Updated description of the BuildEnvironment.")
-@arg("-a", "--attributes", help="Attributes of the BuildEnvironment. Syntax: Key=Value", type=valid_attribute)
-@arg("-iru", "--image-repository-url", help="Updated URL for the Docker repository in which the image resides.")
-@arg("-n", "--name", help="Updated unique name of the BuildEnvironment", type=types.unique_environment_name)
-def update_environment(id, **kwargs):
-    """
-    Update a BuildEnvironment with new information
-    """
-    to_update = envs_api.get_specific(id=id).content
-
-    for key, value in iteritems(kwargs):
-        if value:
-            setattr(to_update, key, value)
-
-    response = utils.checked_api_call(
-        envs_api, 'update', id=id, body=to_update)
-    if response:
-        return utils.format_json(response.content)
-
-
-@arg("-i", "--id", help="ID of the BuildEnvironment to delete.", type=types.existing_environment_id)
-@arg("-n", "--name", help="Name of the BuildEnvironment to delete.", type=types.existing_environment_name)
-def delete_environment(id=None, name=None):
-    """
-    Delete an environment by name or ID
-    """
-    found_id = common.set_id(envs_api, id, name)
-    response = utils.checked_api_call(envs_api, 'delete', id=found_id)
-    return response
-
-
-def get_environment_raw(id=None, name=None):
-    """
-    Get a specific Environment by name or ID
-    """
-    search_id = common.set_id(envs_api, id, name)
-    response = utils.checked_api_call(envs_api, 'get_specific', id=search_id)
-    return response.content
-
-
 @arg("-i", "--id", help="ID of the BuildEnvironment to retrieve.", type=types.existing_environment_id)
 @arg("-n", "--name", help="Name of the BuildEnvironment to retrieve.", type=types.existing_environment_name)
 def get_environment(id=None, name=None):
     """
     Get a specific Environment by name or ID
     """
+    data = get_environment_raw(id, name)
+    if data:
+        return utils.format_json_list(data)
+
+def get_environment_raw(id=None, name=None):
     search_id = common.set_id(envs_api, id, name)
     response = utils.checked_api_call(envs_api, 'get_specific', id=search_id)
-    return utils.format_json(response.content)
+    return response.content
 
 
 @arg("-p", "--page-size", help="Limit the amount of BuildEnvironments returned", type=int)
--- test/unit/test_environments.py
+++ test/unit/test_environments.py
@@ -6,89 +6,14 @@ __author__ = 'thauser'
 from mock import MagicMock, patch
 
 from pnc_cli import environments
-from pnc_cli.swagger_client import BuildEnvironmentRest
 from pnc_cli.swagger_client import EnvironmentsApi
-
-
-def test_create_environment_object():
-    compare = BuildEnvironmentRest()
-    compare.build_type = 'JAVA'
-    compare.name = 'test-environment'
-    result = environments.create_environment_object(name='test-environment', build_type='java')
-    assert result.to_dict() == compare.to_dict()
-
-
-def test_valid_attribute():
-    result = environments.valid_attribute('valid=attribute')
-    assert result == {'valid': 'attribute'}
-
-
-def test_valid_attribute_invalid():
-    with pytest.raises(argparse.ArgumentTypeError):
-        environments.valid_attribute('invalid')
-
-
-@patch('pnc_cli.environments.envs_api.get_all', return_value=MagicMock(content=None))
-def test_unique_iid(mock_get_all):
-    result = environments.unique_iid('unique-image-id')
-    mock_get_all.assert_called_once_with(q='systemImageId==unique-image-id')
-    assert result == 'unique-image-id'
-
-
-@patch('pnc_cli.environments.envs_api.get_all', return_value=MagicMock(content='not-unique'))
-def test_unique_iid_existing(mock_get_all):
-    with pytest.raises(argparse.ArgumentTypeError):
-        environments.unique_iid('not-unique')
-    mock_get_all.assert_called_once_with(q='systemImageId==not-unique')
-
-
-@patch('pnc_cli.environments.create_environment_object', return_value='test-environment-object')
-@patch('pnc_cli.environments.envs_api.create_new', return_value=MagicMock(content='buildenvironment1'))
-def test_create_environment(mock_create_new, mock_create_environment_object):
-    result = environments.create_environment(name='testerino', build_type='JAVA', attributes='test=test')
-    mock_create_environment_object.assert_called_once_with(name='testerino', build_type='JAVA', attributes='test=test')
-    mock_create_new.assert_called_once_with(body='test-environment-object')
-    assert result == 'buildenvironment1'
-
-
-@patch('pnc_cli.environments.envs_api.get_specific')
-@patch('pnc_cli.environments.envs_api.update', return_value=MagicMock(content='buildenvironment1'))
-def test_update_environment(mock_update, mock_get_specific):
-    mock = MagicMock()
-    mockcontent = MagicMock(content=mock)
-    mock_get_specific.return_value = mockcontent
-    result = environments.update_environment(id=1, name='testerino2', build_type='JAVA', attributes='test=test')
-    mock_get_specific.assert_called_once_with(id=1)
-    mock_update.assert_called_once_with(id=1, body=mock)
-    assert getattr(mock, 'build_type') == 'JAVA'
-    assert result == 'buildenvironment1'
-
-
-@patch('pnc_cli.common.set_id', return_value=1)
-@patch('pnc_cli.environments.envs_api.delete', return_value=True)
-@patch('pnc_cli.environments.envs_api', autospec=EnvironmentsApi)
-def test_delete_environment_id(mock_envs_api, mock_delete, mock_set_id):
-    result = environments.delete_environment(id=1)
-    mock_set_id.assert_called_once_with(mock_envs_api, 1, None)
-    mock_delete.assert_called_once_with(id=1)
-    assert result
-
-
-@patch('pnc_cli.common.set_id', return_value=1)
-@patch('pnc_cli.environments.envs_api.delete', return_value=True)
-@patch('pnc_cli.environments.envs_api', autospec=EnvironmentsApi)
-def test_delete_environment_name(mock_envs_api, mock_delete, mock_set_id):
-    result = environments.delete_environment(name='testenv')
-    mock_set_id.assert_called_once_with(mock_envs_api, None, 'testenv')
-    mock_delete.assert_called_once_with(id=1)
-    assert result
 
 
 @patch('pnc_cli.common.set_id', return_value=1)
 @patch('pnc_cli.environments.envs_api.get_specific', return_value=MagicMock(content='buildenvironment1'))
 @patch('pnc_cli.environments.envs_api', autospec=EnvironmentsApi)
 def test_get_environment_id(mock_envs_api, mock_get_specific, mock_set_id):
-    result = environments.get_environment(id=1)
+    result = environments.get_environment_raw(id=1)
     mock_set_id.assert_called_once_with(mock_envs_api, 1, None)
     mock_get_specific.assert_called_once_with(id=1)
     assert result == 'buildenvironment1'
@@ -98,7 +23,7 @@ def test_get_environment_id(mock_envs_api, mock_get_specific, mock_set_id):
 @patch('pnc_cli.environments.envs_api.get_specific', return_value=MagicMock(content='buildenvironment1'))
 @patch('pnc_cli.environments.envs_api', autospec=EnvironmentsApi)
 def test_get_environment_name(mock_envs_api, mock_get_specific, mock_set_id):
-    result = environments.get_environment(name='testerino')
+    result = environments.get_environment_raw(name='testerino')
     mock_set_id.assert_called_once_with(mock_envs_api, None, 'testerino')
     mock_get_specific.assert_called_once_with(id=1)
     assert result == 'buildenvironment1'
@@ -106,6 +31,6 @@ def test_get_environment_name(mock_envs_api, mock_get_specific, mock_set_id):
 
 @patch('pnc_cli.environments.envs_api.get_all', return_value=MagicMock(content=['env1', 'env2', 'env3']))
 def test_list_environments(mock):
-    result = environments.list_environments()
-    mock.assert_called_once_with(page_size=200, q="", sort="")
+    result = environments.list_environments_raw()
+    mock.assert_called_once_with(page_index=0, page_size=200, q="", sort="")
     assert result == ['env1', 'env2', 'env3']
