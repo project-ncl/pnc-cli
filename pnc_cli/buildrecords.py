ce3e0eb32d550e520b2c6853cd5788bdb1c8af73
Honza Brázdil <jbrazdil@redhat.com>
2018-04-12 15:45:56 +0200

    Fix buildrecords tests

--- pnc_cli/buildrecords.py
+++ pnc_cli/buildrecords.py
@@ -21,9 +21,14 @@ def list_build_records(page_size=200, page_index=0, sort="", q=""):
     """
     List all BuildRecords
     """
+    data = list_build_records_raw(page_size, page_index, sort, q)
+    if data:
+        return utils.format_json_list(data)
+
+def list_build_records_raw(page_size=200, page_index=0, sort="", q=""):
     response = utils.checked_api_call(records_api, 'get_all', page_size=page_size, page_index=page_index, sort=sort, q=q)
     if response:
-        return utils.format_json_list(response.content)
+        return response.content
 
 
 @arg("-i", "--id", help="BuildConfiguration ID to retrieve BuildRecords of.", type=types.existing_bc_id)
@@ -36,11 +41,16 @@ def list_records_for_build_configuration(id=None, name=None, page_size=200, page
     """
     List all BuildRecords for a given BuildConfiguration
     """
+    data = list_records_for_build_configuration_raw(id, name, page_size, page_index, sort, q)
+    if data:
+        return utils.format_json_list(data)
+
+def list_records_for_build_configuration_raw(id=None, name=None, page_size=200, page_index=0, sort="", q=""):
     config_id = common.set_id(configs_api, id, name)
     response = utils.checked_api_call(records_api, 'get_all_for_build_configuration', configuration_id=config_id,
                                       page_size=page_size, page_index=page_index, sort=sort, q=q)
     if response:
-        return utils.format_json_list(response.content)
+        return response.content
 
 
 @arg("-i", "--id", help="Project ID to retrieve BuildRecords of.")
@@ -53,11 +63,16 @@ def list_records_for_project(id=None, name=None, page_size=200, page_index=0, so
     """
     List all BuildRecords for a given Project
     """
+    data = list_records_for_project_raw(id, name, page_size, page_index, sort, q)
+    if data:
+        return utils.format_json_list(data)
+
+def list_records_for_project_raw(id=None, name=None, page_size=200, page_index=0, sort="", q=""):
     project_id = common.set_id(projects_api, id, name)
     response = utils.checked_api_call(records_api, 'get_all_for_project_1', project_id=project_id, page_size=page_size, page_index=page_index,
                                       sort=sort, q=q)
     if response:
-        return utils.format_json_list(response.content)
+        return response.content
 
 
 @arg("id", help="BuildRecord ID to retrieve.", type=types.existing_build_record)
@@ -65,9 +80,14 @@ def get_build_record(id):
     """
     Get a specific BuildRecord by ID
     """
+    data = get_build_record_raw(id)
+    if data:
+        return utils.format_json(data)
+
+def get_build_record_raw(id):
     response = utils.checked_api_call(records_api, 'get_specific', id=id)
     if response:
-        return utils.format_json(response.content)
+        return response.content
 
 
 @arg("id", help="BuildRecord ID to retrieve artifacts from.", type=types.existing_build_record)
@@ -79,10 +99,15 @@ def list_built_artifacts(id, page_size=200, page_index=0, sort="", q=""):
     """
     List Artifacts associated with a BuildRecord
     """
+    data = list_built_artifacts_raw(id, page_size, page_index, sort, q)
+    if data:
+        return utils.format_json_list(data)
+
+def list_built_artifacts_raw(id, page_size=200, page_index=0, sort="", q=""):
     response = utils.checked_api_call(records_api, 'get_built_artifacts', id=id,
             page_size=page_size, page_index=page_index, sort=sort, q=q)
     if response:
-        return utils.format_json_list(response.content)
+        return response.content
 
 
 @arg("id", help="BuildRecord ID to retrieve dependency Artifacts from.", type=types.existing_build_record)
@@ -94,10 +119,15 @@ def list_dependency_artifacts(id, page_size=200, page_index=0, sort="", q=""):
     """
     List dependency artifacts associated with a BuildRecord
     """
+    data = list_dependency_artifacts_raw(id, page_size, page_index, sort, q)
+    if data:
+        return utils.format_json_list(data)
+
+def list_dependency_artifacts_raw(id, page_size=200, page_index=0, sort="", q=""):
     response = utils.checked_api_call(records_api, 'get_dependency_artifacts', id=id, page_size=page_size, page_index=page_index, sort=sort,
                                       q=q)
     if response:
-        return utils.format_json_list(response.content)
+        return response.content
 
 
 @arg("id", help="BuildRecord ID to retrieve audited BuildConfiguration from.", type=types.existing_build_record)
@@ -105,9 +135,14 @@ def get_audited_configuration_for_record(id):
     """
     Get the BuildConfigurationAudited for a given BuildRecord
     """
+    data = get_audited_configuration_for_record_raw(id)
+    if data:
+        return utils.format_json(data)
+
+def get_audited_configuration_for_record_raw(id):
     response = utils.checked_api_call(records_api, 'get_build_configuration_audited', id=id)
     if response:
-        return utils.format_json(response.content)
+        return response.content
 
 
 @arg("id", help="BuildRecord ID to retrieve the log from.", type=types.existing_build_record)
@@ -115,6 +150,11 @@ def get_log_for_record(id):
     """
     Get the log for a given BuildRecord
     """
+    data = get_log_for_record_raw(id)
+    if data:
+        return utils.format_json(data)
+
+def get_log_for_record_raw(id):
     response = utils.checked_api_call(records_api, 'get_logs', id=id)
     if response:
         return response
@@ -126,9 +166,14 @@ def get_log_for_record(id):
 @arg("-s", "--sort", help="Sorting RSQL")
 @arg("-q", help="RSQL query")
 def list_artifacts(id, page_size=200, page_index=0, sort="", q=""):
+    data = list_artifacts_raw(id, page_size, page_index, sort, q)
+    if data:
+        return utils.format_json_list(data)
+
+def list_artifacts_raw(id, page_size=200, page_index=0, sort="", q=""):
     response = utils.checked_api_call(records_api, 'get_artifacts', id=id, page_size=page_size, page_index=page_index, sort=sort, q=q)
     if response:
-        return utils.format_json_list(response.content)
+        return response.content
 
 
 @arg("id", help="BuildRecord ID to add an Attribute to.", type=types.existing_build_record)
@@ -147,13 +192,23 @@ def remove_attribute(id, key):
 @arg("key", help="Key of the Attribute to query BuildRecords for.")
 @arg("value", help="Value of the Attribute to query BuildRecords for.")
 def query_by_attribute(key, value):
+    data = query_by_attribute_raw(key, value)
+    if data:
+        return utils.format_json(data)
+
+def query_by_attribute_raw(key, value):
     response = utils.checked_api_call(records_api, "query_by_attribute", key=key, value=value)
     if response:
-        return utils.format_json(response)
+        return response
 
 
 @arg("id", help="BuildRecord ID to list Attributes of.", type=types.existing_build_record)
 def list_attributes(id):
+    data = list_attributes_raw(id)
+    if data:
+        return utils.format_json_list(data)
+
+def list_attributes_raw(id,):
     response = utils.checked_api_call(records_api, 'get_attributes', id=id)
     if response:
         return response.content
--- test/unit/test_buildrecords.py
+++ test/unit/test_buildrecords.py
@@ -10,8 +10,8 @@ from pnc_cli.swagger_client import ProjectsApi
 def test_list_build_records(mock_records_api):
     mock_records_api.get_all = MagicMock(name='get_all_mock',
                                          return_value=MagicMock(name='mock_response', content=[1, 2, 3]))
-    result = buildrecords.list_build_records()
-    mock_records_api.get_all.assert_called_once_with(page_size=200, q="", sort="")
+    result = buildrecords.list_build_records_raw()
+    mock_records_api.get_all.assert_called_once_with(page_index=0, page_size=200, q="", sort="")
     assert result == [1, 2, 3]
 
 
@@ -19,9 +19,9 @@ def test_list_build_records(mock_records_api):
 @patch('pnc_cli.buildrecords.records_api.get_all_for_build_configuration', return_value=MagicMock(content=[2, 3, 4]))
 @patch('pnc_cli.buildrecords.configs_api', autospec=BuildconfigurationsApi)
 def test_list_records_for_build_configuration_id(mock_configs_api, mock_records_api, mock_set_id):
-    result = buildrecords.list_records_for_build_configuration(id='1')
+    result = buildrecords.list_records_for_build_configuration_raw(id='1')
     mock_set_id.assert_called_once_with(mock_configs_api, '1', None)
-    mock_records_api.assert_called_once_with(configuration_id='1', page_size=200, q="", sort="")
+    mock_records_api.assert_called_once_with(configuration_id='1', page_index=0, page_size=200, q="", sort="")
     assert result == [2, 3, 4]
 
 
@@ -29,35 +29,35 @@ def test_list_records_for_build_configuration_id(mock_configs_api, mock_records_
 @patch('pnc_cli.buildrecords.records_api.get_all_for_build_configuration', return_value=MagicMock(content=[1, 2, 3]))
 @patch('pnc_cli.buildrecords.configs_api', autospec=BuildconfigurationsApi)
 def test_list_records_for_build_configuration_name(mock_configs_api, mock_records_api, mock_set_id):
-    result = buildrecords.list_records_for_build_configuration(name='test configuration')
+    result = buildrecords.list_records_for_build_configuration_raw(name='test configuration')
     mock_set_id.assert_called_once_with(mock_configs_api, None, 'test configuration')
-    mock_records_api.assert_called_once_with(configuration_id=2, page_size=200, q="", sort="")
+    mock_records_api.assert_called_once_with(configuration_id=2, page_index=0, page_size=200, q="", sort="")
     assert result == [1, 2, 3]
 
 
 @patch('pnc_cli.common.set_id', autospec=True, return_value='1')
-@patch('pnc_cli.buildrecords.records_api.get_all_for_project', return_value=MagicMock(content=[1]))
+@patch('pnc_cli.buildrecords.records_api.get_all_for_project_1', return_value=MagicMock(content=[1]))
 @patch('pnc_cli.buildrecords.projects_api', autospec=ProjectsApi)
 def test_list_records_for_project_id(mock_projects_api, mock_records_api, mock_set_id):
-    result = buildrecords.list_records_for_project(id='1')
+    result = buildrecords.list_records_for_project_raw(id='1')
     mock_set_id.assert_called_once_with(mock_projects_api, '1', None)
-    mock_records_api.assert_called_once_with(project_id='1', page_size=200, q="", sort="")
+    mock_records_api.assert_called_once_with(project_id='1', page_index=0, page_size=200, q="", sort="")
     assert result == [1]
 
 
 @patch('pnc_cli.common.set_id', autospec=True, return_value='2')
-@patch('pnc_cli.buildrecords.records_api.get_all_for_project', return_value=MagicMock(content=[2]))
+@patch('pnc_cli.buildrecords.records_api.get_all_for_project_1', return_value=MagicMock(content=[2]))
 @patch('pnc_cli.buildrecords.projects_api', autospec=ProjectsApi)
 def test_list_records_for_project_name(mock_projects_api, mock_records_api, mock_set_id):
-    result = buildrecords.list_records_for_project(name='Tom')
+    result = buildrecords.list_records_for_project_raw(name='Tom')
     mock_set_id.assert_called_once_with(mock_projects_api, None, 'Tom')
-    mock_records_api.assert_called_once_with(project_id='2', page_size=200, q="", sort="")
+    mock_records_api.assert_called_once_with(project_id='2', page_index=0, page_size=200, q="", sort="")
     assert result == [2]
 
 
 @patch('pnc_cli.buildrecords.records_api.get_specific', return_value=MagicMock(content='build record 2'))
 def test_get_build_record_id(mock_get_specific):
-    result = buildrecords.get_build_record('2')
+    result = buildrecords.get_build_record_raw('2')
     mock_get_specific.assert_called_once_with(id='2')
     assert result == 'build record 2'
 
@@ -65,37 +65,37 @@ def test_get_build_record_id(mock_get_specific):
 @patch('pnc_cli.buildrecords.records_api.get_built_artifacts',
        return_value=MagicMock(content=['artifact1', 'artifact2']))
 def test_list_build_artifacts(mock):
-    result = buildrecords.list_built_artifacts('1')
-    mock.assert_called_once_with(id='1', page_size=200, q="", sort="")
+    result = buildrecords.list_built_artifacts_raw('1')
+    mock.assert_called_once_with(id='1', page_index=0, page_size=200, q="", sort="")
     assert result == ['artifact1', 'artifact2']
 
 
 @patch('pnc_cli.buildrecords.records_api.get_dependency_artifacts',
        return_value=MagicMock(content=['artifact1', 'artifact2']))
 def test_list_dependency_artifacts(mock):
-    result = buildrecords.list_dependency_artifacts('1')
-    mock.assert_called_once_with(id='1', page_size=200, q="", sort="")
+    result = buildrecords.list_dependency_artifacts_raw('1')
+    mock.assert_called_once_with(id='1', page_index=0, page_size=200, q="", sort="")
     assert result == ['artifact1', 'artifact2']
 
 
 @patch('pnc_cli.buildrecords.records_api.get_build_configuration_audited', return_value=MagicMock(content='audited bc'))
 def test_get_audited_configuration_for_record(mock):
-    result = buildrecords.get_audited_configuration_for_record(id=100)
+    result = buildrecords.get_audited_configuration_for_record_raw(id=100)
     mock.assert_called_once_with(id=100)
     assert result == 'audited bc'
 
 
 @patch('pnc_cli.buildrecords.records_api.get_logs', return_value='log here.')
 def test_get_log_for_record(mock):
-    result = buildrecords.get_log_for_record(id=100)
+    result = buildrecords.get_log_for_record_raw(id=100)
     mock.assert_called_once_with(id=100)
     assert result == 'log here.'
 
 
 @patch('pnc_cli.buildrecords.records_api.get_artifacts', return_value=MagicMock(content="list of artifacts"))
 def test_get_artifacts(mock):
-    result = buildrecords.list_artifacts(id=100)
-    mock.assert_called_once_with(id=100, page_size=200, sort="", q="")
+    result = buildrecords.list_artifacts_raw(id=100)
+    mock.assert_called_once_with(id=100, page_index=0, page_size=200, sort="", q="")
     assert result == "list of artifacts"
 
 @patch('pnc_cli.buildrecords.records_api.put_attribute')
@@ -114,13 +114,13 @@ def test_remove_attribute(mock):
 
 @patch('pnc_cli.buildrecords.records_api.query_by_attribute', return_value=MagicMock(content=MagicMock(attributes={"key":"value"})))
 def test_query_by_attribute(mock):
-    result = buildrecords.query_by_attribute('key', 'value')
+    result = buildrecords.query_by_attribute_raw('key', 'value')
     assert 'key' in result.attributes
     mock.assert_called_once_with(key='key', value='value')
 
 
 @patch('pnc_cli.buildrecords.records_api.get_attributes', return_value=MagicMock(content=MagicMock(attributes={'key1':'value1','key2':'value2'})))
 def test_list_attributes(mock):
-    result = buildrecords.list_attributes(1)
+    result = buildrecords.list_attributes_raw(1)
     assert result.attributes == {'key1':'value1','key2':'value2'}
     mock.assert_called_once_with(id=1)
