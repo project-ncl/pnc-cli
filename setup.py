8fa4562b664a2defa0e0760bfeb45ca2ff5e9c07
Honza Brázdil <jbrazdil@redhat.com>
2018-09-27 10:37:34 +0200

    Release 1.4.0

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 setup(
     name='pnc-cli',
     packages=find_packages(exclude=['test*']),
-    version='1.3.0',
+    version='1.4.0',
     description='CLI for the Project Newcastle build system',
     author = 'Tom Hauser',
     author_email = 'thauser@redhat.com',
     url = 'https://github.com/project-ncl/pnc-cli',
-    download_url='https://github.com/project-ncl/pnc-cli/tarball/pypi-1.3.0',
+    download_url='https://github.com/project-ncl/pnc-cli/tarball/pypi-1.4.0',
     keywords = ['PNC','REST'],
     long_description=README,
     install_requires=[
@@ -24,7 +24,7 @@ setup(
     ],
     tests_require=["pytest >= 2.0"],
     classifiers=[
-	'Development Status :: 3 - Alpha',
+	'Development Status :: 5 - Production/Stable',
 	'Programming Language :: Python :: 2.7',
 	'Programming Language :: Python :: 3'
     ],
